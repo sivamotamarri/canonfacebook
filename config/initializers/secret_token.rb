aa365068fe714fc4897c86988d2d69406cee7572
sivakrishna <sivakrishna.m@imaginea.com>
2012-02-22 12:47:39 +0530

    app on facebook created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Canapp::Application.config.secret_token = '2a441fca44d0dfe7c2f488e838c147642b34435c7b2de4e38eef68e484222d27d86b7645036cfd453d7cc58a6005b6553a1cd85788780f21544396010f359314'
